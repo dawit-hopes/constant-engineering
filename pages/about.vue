8bdd472da9423087ccf916cad0cc5413157f9324
dawit-hopes <dawit.abrahame@gmail.com>
2026-07-10 17:03:16 +0300

    updated: fixed broken image

--- pages/about.vue
+++ pages/about.vue
@@ -414,7 +414,7 @@
           >
             <div class="relative rounded-2xl overflow-hidden shadow-2xl aspect-[4/3] w-full">
               <img
-                src="https://constanteng.com/assets/img/choose%20us.png"
+                src="/Why_choose_us.png"
                 alt="Why Choose Us - CONSTANT ENGINEERING"
                 class="w-full h-full object-cover"
                 loading="lazy"
